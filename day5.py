f3a5353c0ed3437ce0b7085c07ee9cad431096c7
MarceloFirmato <116672694+MarceloFirmato@users.noreply.github.com>
2023-01-18 15:27:59 -0300

    JOGO

--- day5.py
+++
@@ -1,8 +0,0 @@
-fruits = ["Apple", "Peach", "Pear"]
-#fruit é um nome dado as interações
-for fruit in fruits:  
-    print(fruit)
-    # Podemos fazer mais de uma coisa num mesmo loop basta respeitar a indentção
-    print(fruit + " pie")
-# CAso coloque algum código fora da indentação do for ele será processado após o loop ser concluido 
-print(fruits)
